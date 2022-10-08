c943461870ed136cd0da0c651a84a2af88a492be
daniel cortes pichardo <daniel.cortes.pichardo@gmail.com>
2022-10-08 01:07:38 -0500

    meta model

---
+++ src/main/webapp/app/forms/form.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <h1>hola mundo</h1>
+      <div class="alert alert-success">
+        <span><strong>Esto es un test</strong> Please </span>
+      </div>
+      <div class="myform">
+        <json-forms :data="data" :renderers="renderers" :schema="schema" :uischema="uischema" @change="onChange" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" src="./form.component.ts"></script>
